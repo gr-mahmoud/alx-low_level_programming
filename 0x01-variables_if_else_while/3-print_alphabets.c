b2d356a1c5cbf8d9629e5a77a0881edb3a38fd53
gr-mahmoud <vedovedovedo2004@gmail.com>
2023-07-14 18:13:59 -0700

    a

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,22 +10,20 @@
 
 int main(void)
 {
-        char c = 'a';
-        char C = 'A';
+char c = 'a';
+char C = 'A';
 
-        /*prints a = z*/
-        while (c <= 'z')
-        {
-                putchar(c);
-                c++;
-        }
-
-        /*prints A = Z*/
-        while (C <= 'Z')
-        {
-                putchar(C);
-                C++;
-        }
-        putchar('\n');
-        return (0);
+/*prints a = z*/
+while (c <= 'z')
+{
+putchar(c);
+c++;
+/*prints A = Z*/
+while (C <= 'Z')
+{
+putchar(C);
+C++;
+}
+putchar('\n');
+return (0);
 }
